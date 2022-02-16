f9ae7f572d34c41c13afd04ffbe57130fd8a0dfa
Arish-Shah <arish.r.shah@gmail.com>
2022-02-16 19:11:06 +0530

    add guessing game project

---
+++ guessing_game/README.md
@@ -0,0 +1,9 @@
+# guessing_game
+
+1. To add dependency to rust projects, edit the `[dependencies]` property.
+
+```rust
+[dependencies]
+rand = "0.8.5"
+```
+2. Run `cargo build` to fetch the packages from _registry_.
---
+++ guessing_game/src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number: u32 = rand::thread_rng().gen_range(1..101);
+    
+    loop {
+        let mut guess = String::new();
+
+        println!("Enter your guess.");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Unable to read user input");
+
+        let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too less!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
